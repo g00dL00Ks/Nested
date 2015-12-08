f74f2c97f87bce690ae70c8f596ca834d6f79309
Mitzi Reaugh <mitzi@goodlooks.me>
2015-12-08 15:56:40 -0800

    First attempt at adding bookings
    not completely working yet for the bookings experience

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :bookings
   resources :pros do
     get "results", on: :collection
   end
 
+  get 'quiz' => "pros#quiz"
+
+  get 'account' => "pros#account"
+
+  get 'booking' => "bookings#index"
+
+  get 'booking/new' => "bookings#new"
+
   root 'pros#index'
+
+
 end
